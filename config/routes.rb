3685bcea8ba45f76964dd54ed2162b7ecd1c1766
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2025-04-21 07:13:51 +0900

    [Add] admin/homesコントローラーの作成、ルーティングの編集

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,8 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
+  namespace :admin do
+    root to: "homes#top"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
