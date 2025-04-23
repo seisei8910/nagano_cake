b128366dbd8fab400dcaa81460cb10003a77facc
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2025-04-24 06:17:23 +0900

    [Add] customersコントローラーのshowアクションに記述

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -2,6 +2,7 @@ class Public::CustomersController < ApplicationController
   before_action :authenticate_customer!
 
   def show
+    @customer = Customer.find(current_customer.id)
   end
 
   def edit
