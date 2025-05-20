14b616b2751354d06027f420bbbe2302e518e58b
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2025-05-21 07:28:59 +0900

    [Add] genresコントローラーのupdateアクションを記述、更新機能を追加

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -5,13 +5,19 @@ class Admin::GenresController < ApplicationController
   end
 
   def create
-    @genre = Genre.new(genre_params)
-    @genre.save
+    genre = Genre.new(genre_params)
+    genre.save
     redirect_to admin_genres_path
   end
 
   def edit
     @genre = Genre.find(params[:id])
+  end
+
+  def update
+    genre = Genre.find(params[:id])
+    genre.update(genre_params)
+    redirect_to admin_genres_path
   end
 
   private
