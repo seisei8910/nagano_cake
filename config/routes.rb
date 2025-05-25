f5ae1377a558942c43678d075709cbcd3ca8895e
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2025-05-25 09:22:29 +0900

    [Add] admin/itemsのルーティングの編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/show'
-    get 'items/edit'
-  end
   # 顧客用
   # URL /customers/sign_in ...
   devise_for :customers, skip: [:passwords], controllers: {
@@ -33,6 +27,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: "homes#top"
     resources :genres, only: [:index, :create, :edit, :update]
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
