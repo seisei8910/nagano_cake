dbe05472c1680b208131e391136189dd47ab0e66
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2025-05-25 09:16:17 +0900

    [Add] admin/itemsコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'items/index'
+    get 'items/new'
+    get 'items/show'
+    get 'items/edit'
+  end
   # 顧客用
   # URL /customers/sign_in ...
   devise_for :customers, skip: [:passwords], controllers: {
