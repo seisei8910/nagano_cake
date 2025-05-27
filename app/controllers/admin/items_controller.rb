82a0116ee68dd42708c4bb3979440ea20f52054e
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2025-05-27 10:10:20 +0900

    [Add] 商品登録機能実装、画像保存機能実装

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -6,9 +6,22 @@ class Admin::ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_item_path(@item.id)
+  end
+
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction,
+    :genre_id, :price, :is_active)
   end
 end
