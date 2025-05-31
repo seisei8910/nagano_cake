3865ea019c4f897208310b8b873cc14d0da8630a
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2025-05-31 18:02:47 +0900

    [Add] item更新機能追加

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -11,9 +11,9 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item_params)
-    @item.save
-    redirect_to admin_item_path(@item.id)
+    item = Item.new(item_params)
+    item.save
+    redirect_to admin_item_path(item.id)
   end
 
   def show
@@ -22,6 +22,12 @@ class Admin::ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to admin_item_path(item.id)
   end
 
   private
