94240c558f30178102009ac63bca082f4a4b36da
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2025-06-11 15:41:21 +0900

    [Add] 顧客側ジャンル検索機能実装

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -3,6 +3,10 @@ class Public::ItemsController < ApplicationController
     if params[:name].present?
       @items = Item.where("name LIKE ?", "%#{params[:name]}%").page(params[:page])
       @genres = Genre.all
+    elsif params[:genre_name].present?
+      genre = Genre.find_by(name: params[:genre_name])
+      @items = Item.where(genre_id: genre.id).page(params[:page])
+      @genres = Genre.all
     else
       @items = Item.page(params[:page]).per(8)
       @genres = Genre.all
